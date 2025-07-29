218aa8206a0df1b161d389b7c3d4fda170ddc156
Adel Bakhshi <adelbakhshi78@yahoo.com>
2025-07-29 14:39:11 +0330

    Change in media popup for better readability and performance

--- firefox-extension/media-popup/script.js
+++ firefox-extension/media-popup/script.js
@@ -1,24 +1,42 @@
+// Define popup elements
 let popup;
 let popupHeader;
 let popupMenu;
 
+// Define popup buttons
 let closePopupButton;
 let showHideMenuButton;
 
-// Find all video and audio elements
+// Define timer for search
+let searchTimer;
+
+/**
+ * Finds all media elements (video and audio) in the document and retrieves their sources.
+ *
+ * This function searches the document for all video and audio elements, collects their
+ * current source URLs, and also checks for nested source elements within these media elements.
+ * It then filters out duplicate sources to ensure each source is only listed once in the result.
+ *
+ * @returns {Array} An array of objects, each containing the 'source' URL and the corresponding 'element'.
+ */
 function findMediaElements() {
+  // Select all video and audio elements in the document
   const mediaElements = document.querySelectorAll("video, audio");
 
-  // Get video and audio sources
-  const result = [];
+  // Initialize an array to store the sources of the media elements
+  const data = [];
   for (const element of mediaElements) {
+    // Check the tag name of the element to determine if it's a video or audio element
     switch (element.tagName.toLowerCase()) {
       case "video":
       case "audio": {
-        result.push({ source: element.currentSrc, element: element });
+        // Add the current source of the element to the data array
+        data.push({ source: element.currentSrc, element: element });
+        // Find all source elements within the current media element
         const sourceElements = element.querySelectorAll("source");
         for (const sourceElement of sourceElements) {
-          result.push({ source: sourceElement.src, element: element });
+          // Add each source element's src attribute to the data array
+          data.push({ source: sourceElement.src, element: element });
         }
 
         break;
@@ -26,11 +44,24 @@ function findMediaElements() {
     }
   }
 
-  // Distinct sources
-  return result.filter((item, index) => result.indexOf(item) === index);
+  // Initialize an array to store distinct sources
+  const result = [];
+  for (const item of data) {
+    // Check if the source is already in the result array to avoid duplicates
+    if (!result.some((el) => el.source === item.source)) {
+      result.push(item);
+    }
+  }
+
+  // Return the array of distinct sources
+  return result;
 }
 
-// Create popup
+/**
+ * Creates a popup element with a header, menu, and action buttons.
+ * The popup includes a logo, a message, a show/hide menu button, and a close button.
+ * The show/hide button and close button use SVG icons for their visuals.
+ */
 function createPopup() {
   // Create popup
   const popupDiv = document.createElement("div");
@@ -117,7 +148,23 @@ function createPopup() {
   document.body.appendChild(popupDiv);
 }
 
-// Create menu items
+/**
+ * Creates menu items based on the provided media elements.
+ *
+ * @param {Array} mediaElements - An array of objects containing data for each media element.
+ *   Each object should have properties that define the media element.
+ * @example
+ * // Example mediaElements array
+ * const mediaElements = [
+ *   { name: "Home", link: "#" },
+ *   { name: "About", link: "#" },
+ *   { name: "Contact", link: "#" }
+ * ];
+ *
+ * This function will clear the existing menu items in the popupMenu element and create new menu items
+ * based on the provided mediaElements array. Each menu item will be created using the createMenuItem
+ * function, passing the element data and its index (plus one) as arguments.
+ */
 function createMenuItems(mediaElements) {
   // Clear menu
   popupMenu.innerHTML = "";
@@ -128,8 +175,17 @@ function createMenuItems(mediaElements) {
   });
 }
 
-// Create menu item
+/**
+ * Creates a menu item for a given element data and row number.
+ *
+ * @param {Object} elementData - The data for the element, including the source and the element reference.
+ * @param {number} row - The row number for the menu item.
+ */
 function createMenuItem(elementData, row) {
+  // Get the name of the source
+  const mediaName = (getFileName(elementData.source) ?? elementData.source)?.trim();
+  if (!mediaName) return;
+
   // Create list item
   const listItem = document.createElement("li");
   listItem.setAttribute("data-source", elementData.source);
@@ -143,7 +199,7 @@ function createMenuItem(elementData, row) {
 
   // Create row text
   const rowText = document.createElement("span");
-  rowText.textContent = getFileName(elementData.source) ?? elementData.source;
+  rowText.textContent = mediaName;
 
   // Create navigate to media element button
   const navigateButton = document.createElement("button");
@@ -178,8 +234,7 @@ function createMenuItem(elementData, row) {
     });
   });
 
-  // Find popup menu
-  const popupMenu = document.getElementById("cdm-popup-menu");
+  // Check if popup menu exists
   if (!popupMenu) {
     return;
   }
@@ -188,7 +243,15 @@ function createMenuItem(elementData, row) {
   popupMenu.appendChild(listItem);
 }
 
-// Menu item on click
+/**
+ * Handles the click event for a menu item.
+ *
+ * This function is triggered when a menu item is clicked. It retrieves the menu item,
+ * extracts the source URL from it, sends a message to the Chrome runtime to initiate
+ * a download, and then hides the popup menu.
+ *
+ * @param {Event} event - The click event triggered by the user.
+ */
 async function menuItemOnClick(event) {
   // Get menu item
   let menuItem = event.target;
@@ -196,12 +259,14 @@ async function menuItemOnClick(event) {
     menuItem = menuItem.parentElement;
   }
 
+  // If no menu item is found, exit the function
   if (!menuItem) {
     return;
   }
 
   // Get source
   const source = menuItem.getAttribute("data-source");
+  // If no source attribute is found, exit the function
   if (!source) {
     return;
   }
@@ -216,7 +281,17 @@ async function menuItemOnClick(event) {
   showHideMenuButton.click();
 }
 
-// Get file name
+/**
+ * Extracts the file name from a given source URL or path.
+ *
+ * @param {string} source - The source URL or path from which to extract the file name.
+ * @returns {string} The extracted file name with any URL parameters and percent-encoded spaces removed.
+ *
+ * This function performs the following steps:
+ * 1. Splits the source string by '/' and retrieves the last element (the file name).
+ * 2. If the file name contains a '?', it splits the file name by '?' and takes the first part (removing URL parameters).
+ * 3. Replaces any '%' characters in the file name with spaces (decoding percent-encoded spaces).
+ */
 function getFileName(source) {
   let fileName = source.split("/").pop();
   if (fileName.includes("?")) {
@@ -230,8 +305,13 @@ function getFileName(source) {
   return fileName;
 }
 
+/**
+ * Function to find and manipulate elements related to a popup.
+ * This function locates the popup, its header, and menu, as well as the buttons for closing and showing/hiding the menu.
+ * It also sets up event listeners for these buttons to handle the popup's behavior.
+ */
 function findElements() {
-  // Find popup
+  // Find popup elements
   popup = document.getElementById("cdm-popup");
   popupHeader = document.getElementById("cdm-popup-header");
   popupMenu = document.getElementById("cdm-popup-menu");
@@ -240,21 +320,21 @@ function findElements() {
   closePopupButton = document.getElementById("close-cdm-popup");
   showHideMenuButton = document.getElementById("show-hide-cdm-menu");
 
-  // Close popup
+  // Event listener for closing the popup
   closePopupButton.addEventListener("click", function () {
     popup.classList.remove("show");
 
-    // Set interval for removing popup  after 1 second
+    // Set interval for removing popup after 1 second
     const removePopup = setTimeout(function () {
       popup.remove();
       clearTimeout(removePopup);
     }, 1000);
 
-    // Clear interval
+    // Clear interval (if any) related to search functionality
     clearInterval(searchTimer);
   });
 
-  // Show/Hide menu
+  // Event listener for showing/hiding the menu
   showHideMenuButton.addEventListener("click", function () {
     if (popupMenu.classList.contains("show")) {
       this.classList.remove("reverse");
@@ -268,7 +348,13 @@ function findElements() {
   });
 }
 
-// Initialize popup
+/**
+ * Initializes the popup menu with media elements.
+ *
+ * This function searches for media elements on the page, creates a popup if it doesn't exist,
+ * populates the popup with menu items based on the found media elements, and displays the popup
+ * if there are items to show.
+ */
 function initializePopup() {
   try {
     // Find media elements and get sources
@@ -283,20 +369,26 @@ function initializePopup() {
       findElements();
     }
 
-    // Create menu items
+    // Create menu items based on the found media elements
     createMenuItems(mediaElements);
 
-    // Show popup
-    setTimeout(() => {
-      popup.classList.add("show");
-    }, 1000);
+    // Show popup if there are menu items
+    var items = popupMenu.querySelectorAll("li");
+    if (items.length > 0) {
+      setTimeout(() => {
+        popup.classList.add("show");
+      }, 1000);
+    }
   } catch (e) {
     console.error(e);
   }
 }
 
-// Initialize popup
-initializePopup();
+// Initialize the popup when the content loaded successfully
+(function () {
+  // Initialize popup
+  initializePopup();
 
-// Search for media elements every 10 seconds
-const searchTimer = setInterval(initializePopup, 10000);
+  // Search for media elements every 1 minute
+  searchTimer = setInterval(initializePopup, 60 * 1000);
+})();
